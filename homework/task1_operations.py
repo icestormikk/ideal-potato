270d1ccc486869f51a501608b511c692d279f117
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-11-19 01:26:21 +0300

    Delete task1_operations.py

--- homework/task1_operations.py
+++
@@ -1,5 +0,0 @@
-a,b = float(input('Введите первое число: ')), float(input('Введите второе число: '));
-print(f'a + b == {a + b}\na - b == {a - b}\na * b == {a * b}\na ^ b == {a ** b}')
-
-if (b == 0): print("[!] Операции a / b, a // b, a % b выполнить невозможно: число b равно нулю")
-else: print(f'a / b == {a / b}\na % b == {a % b}\na // b == {a // b}');
