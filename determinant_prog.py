881ed0f57d5c683a1fde5d74f3ef37fb7be66147
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-11-18 18:55:04 +0300

    Add files via upload

---
+++ determinant_prog.py
@@ -0,0 +1,24 @@
+import random
+
+def gauss(matr):
+    for i in range(len(matr)):
+        if (matr[i][i] == 0): continue
+        for j in range(i+1, len(matr[i])):
+            c = matr[j][i] / matr[i][i]
+            for k in range(i, len(matr[i])):
+                matr[j][k] -= c * matr[i][k]
+    return matr
+
+def determinant(matr):
+    result = 1.0; gauss(matr); 
+    for i in range(len(matr)):
+        result *= matr[i][i];
+    return round(result)
+
+size = random.randint(1,10);
+mas = [[0] * size for i in range(size)];
+for i in range(len(mas)):
+    for j in range(len(mas[i])):
+        mas[i][j] = random.randint(1,10);
+print("Matrix:", mas)
+print("Determinant is", determinant(mas))
