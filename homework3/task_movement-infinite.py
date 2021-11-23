8c4a0d274fd5b1846d94c9a532a682bf7d33bd79
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-11-23 21:16:15 +0300

    Create task_movement-infinite.py

---
+++ homework3/task_movement-infinite.py
@@ -0,0 +1,17 @@
+print("Начальное положение персонажа: [0, 0]\nВведите направление движения (left/right/up/down):");
+cur = [0, 0];
+while True:
+    dir = input(); prev = [cur[0], cur[1]]
+    if (dir == "left"):
+        cur[0] -= 1;
+    elif (dir == "right"):
+        cur[0] += 1;
+    elif (dir == "down"):
+        cur[1] -= 1;
+    elif (dir == "up"):
+        cur[1] += 1;
+    else:
+        if (dir != "stop"):
+            print("[!] Не удалось распознать направление", dir);
+        break;
+    print(prev, "->",cur)
