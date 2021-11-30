863aed54694ee98fea4724e14f3bfcc31e9d8a97
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-01 00:09:04 +0300

    Create task1_password.py

---
+++ homework5/task1_password.py
@@ -0,0 +1,37 @@
+def checkPassword(password):
+    if (len(password) < 7):
+        raise ValueError("Слишком короткий пароль.");
+    elif ({x for x in password}.intersection({str(i) for i in range(10)}).__len__() == 0):
+        #если множество из символов принятой строки не пересекается с множеством из цифр, то возвращаем False (или бросаем исключение)
+        raise ValueError("Требуется как минимум одна цифра.");
+    elif (str(password).isnumeric()):
+        raise ValueError("Пароль не может состоять только из цифр.");
+    elif (str(password).lower().__contains__("password")):
+        raise ValueError("Пароль не может содержать слово \"password\".");
+    else:
+        return True;
+
+def isPassword(password):
+    if (len(password) < 7):
+        return False;
+    elif ({x for x in password}.intersection({str(i) for i in range(10)}).__len__() == 0):
+        return False;
+    elif (str(password).isnumeric()):
+        return False;
+    elif (str(password).lower().__contains__("password")):
+        return False;
+    else:
+        return True;
+
+#вариант с true|false
+if (isPassword(input('Введите строку-пароль (true/false version): '))):
+    print("Данная строка может быть паролем");
+else:
+    print("[!] Строка не может использоваться в качестве пароля")
+
+#вариант с исключениями
+try:
+    checkPassword(input('\nВведите строку-пароль (exceptions version): '))
+    print("Данная строка может быть паролем");
+except ValueError as ve:
+    print("[!] Строка не может использоваться в качестве пароля\nПричина:",ve.args[0])
