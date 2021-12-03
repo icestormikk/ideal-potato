92d991e0add0ddab96298b4381ed8a0ebd75daa4
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-03 23:21:14 +0300

    Create task1_byte-list.py

---
+++ homework6/task1_byte-list.py
@@ -0,0 +1,35 @@
+def encodeStrings(lines):
+    result = []
+    for i in lines:
+        if (len(i) != 0):
+            result.append(i.encode('utf-8'))
+    return result
+
+def decodeStrings(lines):
+    result = []
+    for i in lines:
+        if (len(i) != 0):
+            result.append(i.decode('utf-8'))
+    return result
+
+encodedStrings = []
+try:
+    with open(input("Введите путь к файлу со строками: "), "r", encoding='utf-8') as f:
+        encodedStrings = encodeStrings(f.read().split("\n"))
+    print("Результат перевода:", encodedStrings)
+except FileNotFoundError:
+    print("[!] Не удалось найти файл по введённому пути.\n")
+
+with open("bytes.bin", "wb") as f:
+    for i in encodedStrings:
+        f.write(i)
+
+if (len(encodedStrings) != 0):
+    if (input("\nЖелаете декодировать строки с предыдущего шага? (y/n) ") == 'y'):
+        print("Результат обратного перевода", decodeStrings(encodedStrings)); exit()
+
+try:
+    with open(input("Введите путь к файлу со строками байтов: "), "rb") as f:
+        print("Результат обратного перевода:", decodeStrings(f.readlines()))
+except FileNotFoundError:
+    print("[!] Не удалось найти файл по введённому пути.")
