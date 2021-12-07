52523fe9da6def82ddd481ff83d66184936215ce
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-07 13:13:43 +0300

    Update task1_operations.py

--- homework2/task1_operations.py
+++ homework2/task1_operations.py
@@ -1,5 +1,8 @@
-a,b = float(input('Введите первое число: ')), float(input('Введите второе число: '));
+a = float(input('Введите первое число: '))
+b = float(input('Введите второе число: '))
 print(f'a + b == {a + b}\na - b == {a - b}\na * b == {a * b}\na ^ b == {a ** b}')
 
-if (b == 0): print("[!] Операции a / b, a // b, a % b выполнить невозможно: число b равно нулю")
-else: print(f'a / b == {a / b}\na % b == {a % b}\na // b == {a // b}');
+if (b == 0): 
+    print("[!] Операции a / b, a // b, a % b выполнить невозможно: число b равно нулю")
+else: 
+    print(f'a / b == {a / b}\na % b == {a % b}\na // b == {a // b}');
