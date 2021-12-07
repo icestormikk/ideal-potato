fc785d3f7155c4ddefdf1431a43672106a492238
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-07 14:32:46 +0300

    Update task4_inventory.py

--- homework4/task4_inventory.py
+++ homework4/task4_inventory.py
@@ -1,8 +1,24 @@
 maxWeight = 90
-inventory = [["Witcher's steel sword", 1, 1.74], ["Wolf School Medallion", 1, 0.5], ["Swallow", 10, 0.1], ["A letter (with the scent of lilac and gooseberries)", 1, 0.0],
-["Hunting gauntlets", 1, 0.67], ["Kaer Morhen armor", 1, 1.57], ["Hunting trousers", 1, 1.06], ["Hunting boots", 1, 0.71]]
+inventory = [
+    ["Witcher's steel sword", 1, 1.74], 
+    ["Wolf School Medallion", 1, 0.5], 
+    ["Swallow", 10, 0.1], 
+    ["A letter (with the scent of lilac and gooseberries)", 1, 0.0],
+    ["Hunting gauntlets", 1, 0.67], 
+    ["Kaer Morhen armor", 1, 1.57], 
+    ["Hunting trousers", 1, 1.06], 
+    ["Hunting boots", 1, 0.71]
+]
 
-def duplicateItem(name, weigth = None):
+def duplicateItem(name, weigth=None):
+    """Проверка на содержание в инвентаре предметов с тем же именем
+    и весом(если передан). 
+    
+    Аргументы:\n
+    name - название предмета;\n
+    weigth - вес предмета (по умолчанию None)
+    """
+
     for i in inventory:
         if (i[0] == name):
             if (weigth != None): #если передан вес предмета
@@ -16,6 +32,8 @@ def duplicateItem(name, weigth = None):
     return False
 
 def totalInventoryWeight():
+    """Расчёт общего веса инвентаря."""
+
     result = 0
     for i in inventory:
         result += i[1] * i[2] #вес одного предмета * кол-во таких предметов
@@ -24,7 +42,8 @@ def totalInventoryWeight():
 while True:
     user_in = input('Введите действие, которое необходимо выполнить (addItem, removeItem, showInventory, exit): ').lower()
     if (user_in == "additem"):
-        name = input('\tВведите название нового предмета: '); weight = float(input('\tВес предмета: '))
+        name = input('\tВведите название нового предмета: ')
+        weight = float(input('\tВес предмета: '))
         if (totalInventoryWeight() + weight <= maxWeight):
             if (duplicateItem(name, weight)):
                 duplicateItem(name)[1] += 1 #если в инвентаре уже есть такой предмет, увеличиваем кол-во такого предмета на 1
@@ -43,10 +62,13 @@ while True:
     elif (user_in == "showinventory"):
         if (len(inventory) > 0):
             maxLength = 0;
+            #расчёт наибольшей длины строки с названием предмета
             for i in inventory:
                 if (len(i[0]) > maxLength):
                     maxLength = len(i[0])
 
+            #вывод содержимого инвентаря в виде таблицы со столбцами:
+            #"Название предмета"  "Кол-во копий"  "Общий вес"(вес 1-ой единицы * кол-во)
             print("%s%-8s%-6s" % ("Item Name".ljust(maxLength + 4), "Count", "Weight"))
             for item in inventory:
                 print("%s%-8s%-6s" % (item[0].ljust(maxLength + 4), item[1], item[2]))
