66ec9924b646f93b4b26c328f3fd06e9f01984f0
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-07 13:59:26 +0300

    Update task3_random-lists.py

--- homework4/task3_random-lists.py
+++ homework4/task3_random-lists.py
@@ -1,6 +1,7 @@
 import random
 
-list1, list2 = set(random.randint(1,50) for x in range(random.randint(5,20))), set(random.randint(1,50) for x in range(random.randint(5,20)))
+list1 = set(random.randint(1,50) for x in range(random.randint(5,20)))
+list2 = set(random.randint(1,50) for x in range(random.randint(5,20)))
 print(f'Первое множество: {list1}\nВторое множество: {list2}\n')
 
 print(f'Элементы, содержащиеся в обоих множествах одновременно: {list1 & list2}')
