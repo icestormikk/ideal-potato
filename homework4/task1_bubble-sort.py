bd3461108b061039883e930b1e3c6446b008e5e5
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-07 13:45:54 +0300

    Update task1_bubble-sort.py

--- homework4/task1_bubble-sort.py
+++ homework4/task1_bubble-sort.py
@@ -1,12 +1,18 @@
 import random;
 
 def bubble_sort(mass):
+    """Сортировка массива методом пузырька.
+   
+    Аргументы: 
+    mass - массив, который необходимо отсортировать
+    """
+    
     for i in range(len(mass) - 1):
         for j in range(len(mass) - i - 1):
-            if (mass[j] > mass[j+1]):
-                mass[j+1], mass[j] = mass[j], mass[j+1];
+            if (mass[j] > mass[j + 1]):
+                mass[j + 1], mass[j] = mass[j], mass[j + 1];
 
-list = [(random.random() * 100) for x in range(random.randint(5,20))]
+list = [(random.random() * 100) for x in range(random.randint(5, 20))]
 print(f'Исходный массив: {list}')
 bubble_sort(list)
 print(f'\nОтсортированный массив: {list}')
