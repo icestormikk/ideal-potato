dc47e1617875f0debb99ba1d4cd645ca112d7813
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-07 13:46:38 +0300

    Update task_movement-infinite.py

--- homework3/task_movement-infinite.py
+++ homework3/task_movement-infinite.py
@@ -4,15 +4,15 @@ while True:
     dir = input()
     prev = [cur[0], cur[1]] #положение персонажа с предыдущего шага
     if (dir == "left"):
-        cur[0] -= 1;
+        cur[0] -= 1
     elif (dir == "right"):
-        cur[0] += 1;
+        cur[0] += 1
     elif (dir == "down"):
-        cur[1] -= 1;
+        cur[1] -= 1
     elif (dir == "up"):
-        cur[1] += 1;
+        cur[1] += 1
     else:
         if (dir != "stop"):
-            print("[!] Не удалось распознать направление", dir);
-        break;
+            print("[!] Не удалось распознать направление", dir)
+        break
     print(prev, "->",cur)
