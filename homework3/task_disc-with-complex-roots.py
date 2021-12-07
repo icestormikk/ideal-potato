29cec97b42d5cec8c5fb12038b5caca108a4bb69
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-07 13:37:04 +0300

    Update task_disc-with-complex-roots.py

--- homework3/task_disc-with-complex-roots.py
+++ homework3/task_disc-with-complex-roots.py
@@ -1,6 +1,11 @@
 import math;
 def discriminant(coefs):
-    #coef[0] == a, coef[1] == b, coef[2] = c
+    """ Дискриминант квадратного уравнения.
+    
+    Аргументы: 
+    coeffs - список/кортеж значений коэффициентов квадратного уравнения
+    """
+    
     return float(coefs[1]) ** 2  - 4 * float(coefs[0]) * float(coefs[2])
 
 coeffs = [float(x) for x in input("Введите коэффициенты квадратного уравнения (через пробел): ").split(" ")];
@@ -11,8 +16,13 @@ if (coeffs[0] == 0 or len(coeffs) != 3):
 
 print(f'Дискриминант равен {discriminant(coeffs)}\nКорни уравнения: ')
 if (discriminant(coeffs) >= 0):
-    print(f'x1 == {(-coeffs[1] + math.sqrt(discriminant(coeffs))) / (2 * coeffs[0])}', end='');
+    # x1,2 == (-b +- sqrt(discriminant())) / (2 * a)
+    x1 = (-coeffs[1] + math.sqrt(discriminant(coeffs))) / (2 * coeffs[0])
+    print(f'Корни уравнения: x1 == {x1}', end='');
     if (discriminant(coeffs) > 0):
-        print(f', x2 == {(-coeffs[1] - math.sqrt(discriminant(coeffs))) / (2 * coeffs[0])}')
+        x2 = (-coeffs[1] - math.sqrt(discriminant(coeffs))) / (2 * coeffs[0])
+        print(f', x2 == {x2}')
 else:
-    print(f'x1 == {complex(-coeffs[1], math.sqrt(abs(discriminant(coeffs)))) / (2 * coeffs[0])}, x2 == {complex(-coeffs[1], math.sqrt(abs(discriminant(coeffs))) * (-1.0)) / (2 * coeffs[0])}')
+    x1_complex = complex(-coeffs[1], math.sqrt(abs(discriminant(coeffs)))) / (2 * coeffs[0])
+    x2_complex = complex(-coeffs[1], math.sqrt(abs(discriminant(coeffs))) * (-1.0)) / (2 * coeffs[0])
+    print(f'x1 == {x1_complex}, x2 == {x2_complex}')
