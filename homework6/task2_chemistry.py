26d9c3597a90dcf66e3d03497c9780ab5dbb7ac4
icestormikk <94618632+icestormikk@users.noreply.github.com>
2021-12-07 15:59:50 +0300

    Update task2_chemistry.py

--- homework6/task2_chemistry.py
+++ homework6/task2_chemistry.py
@@ -4,20 +4,29 @@ import re
 #Формат ввода: X = Y (X - символ химического элемента, Y - кол-во атомов элемента X)
 #Разделитель: запятая
 #Пример: C = 2, H = 6, O = 1
-elements = []; elements_coeffs = dict()
-with open("input.txt", "rt") as f:
-    elements = re.split(",\W*", f.read());
+elements = []
+elements_coeffs = dict()
+
+try:
+    with open("input.txt", "rt") as f:
+        elements = re.split(",\W*", f.read())
+except FileNotFoundError:
+    print("[!] Не найден файл с именем input.txt")
+    exit()
 
 if (len(elements) == 3):
     for i in elements:
         if (re.fullmatch("[C,H,O]\s*=\s*[0-9]+", i)):
             elements_coeffs[i.replace(" ","").split("=")[0]] = int(i.replace(" ","").split("=")[1])
         else:
-            print("[!] Нарушен формат ввода.\n    Возможные ошибки:\n    (1) Введён иной химический элемент (не С, H или O)\n    (2) Неправильный ввод кол-ва атомов элемента")
+            print("[!] Нарушен формат ввода.)")
+            print("Возможные ошибки:\n    (1) Введён иной химический элемент (не С, H или O)")
+            print("    (2) Неправильный ввод кол-ва атомов элемента")
             print("    (3) Введён лишний разделитель")
             exit()
     if (len(elements_coeffs) != len(elements)):
-        print("[!] Нарушен формат ввода.\n    Возможные ошибки:\n    (1) Переопределено кол-во некоторого элемента")
+        print("[!] Нарушен формат ввода.\n    Возможные ошибки:\n")
+        print("    (1) Переопределено кол-во некоторого элемента")
         exit()
 else:
     print("[!] Ошибка ввода: ", end='')
